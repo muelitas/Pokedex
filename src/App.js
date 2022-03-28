43e43cb3b583b5c75a50cf3eb0fba2a80e373a19
javes <javes@pop-os.localdomain>
2022-03-28 08:33:37 -0600

    Edited default content.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MainLeftCol from "./mainLeftCol";
+import PokDetails from "./components/pokDetails";
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      selectedPok: 'pikachu'
+    };
+
+    this.onClickOfPok = this.onClickOfPok.bind(this);
+  }
+
+  onClickOfPok(selectedPok){
+    this.setState({
+      selectedPok: selectedPok
+    });
+  }
+
+  render(){
+    return(
+      <>
+      <Container>
+        <Row className='text-center'>
+          <Col sm="12">
+            <h1>Pokedex</h1>
+          </Col>
+        </Row>
+        <Row className='text-center'>
+          <Col sm="6">
+            <MainLeftCol 
+              onClick = {this.onClickOfPok}
+              selectedPok = {this.state.selectedPok}
+            />
+          </Col>
+          <Col sm="6">
+            <PokDetails 
+              pokName = {this.state.selectedPok}
+            />
+          </Col>
+        </Row>
+      </Container>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {client} from "./settings/apollo";
+import { ApolloProvider } from '@apollo/client';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +20,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
